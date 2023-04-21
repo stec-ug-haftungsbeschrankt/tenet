57e7224887a028d81765a6a40a3548684761e4a9
Stefan Schick <stefan.schick@stecug.de>
2023-04-21 23:03:23 +0200

    Make everything public

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,12 @@ mod storage;
 mod tenant;
 mod user;
 
-
-use error::TenetError;
-use storage::*;
-use tenant::*;
+pub use application::*;
+pub use error::*;
+pub use role::*;
+pub use storage::*;
+pub use tenant::*;
+pub use user::*;
 
 
 #[derive(Debug, Clone)]
@@ -21,32 +23,32 @@ pub struct Tenet {
 
 
 impl Tenet {
-    fn new(storage: Storage) -> Self {
+    pub fn new(storage: Storage) -> Self {
         Tenet { 
             storage,
             tenants: Vec::new() 
         }
     }
 
-    fn get_tenant_id_by_username(&self, username: String) -> Result<uuid::Uuid, TenetError> {
+    pub fn get_tenant_id_by_username(&self, username: String) -> Result<uuid::Uuid, TenetError> {
         if let Some(tenant) = self.tenants.iter().find(|t| t.contains_username(username.clone())) {
             return Ok(tenant.id);
         }
         Err(TenetError::NotFoundError)
     }
 
-    fn get_tenant_by_username(&self, username: String) -> Result<&Tenant, TenetError> {
+    pub fn get_tenant_by_username(&self, username: String) -> Result<&Tenant, TenetError> {
         if let Some(tenant) = self.tenants.iter().find(|t| t.contains_username(username.clone())) {
             return Ok(tenant);
         }
         Err(TenetError::NotFoundError)
     }
 
-    fn get_tenant_by_id(&self, tenant_id: uuid::Uuid) -> Option<&Tenant> {
+    pub fn get_tenant_by_id(&self, tenant_id: uuid::Uuid) -> Option<&Tenant> {
         self.tenants.iter().find(|t| t.id == tenant_id)
     }
 
-    fn create_tenant(&mut self) -> Result<&mut Tenant, TenetError> {
+    pub fn create_tenant(&mut self) -> Result<&mut Tenant, TenetError> {
         let tenant = Tenant::new();
         self.tenants.push(tenant);
         
@@ -56,22 +58,11 @@ impl Tenet {
 
 
 
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use crate::{application::{Application, ApplicationType}, role::{RoleType, Role}, user::{User, EncryptionModes}};
 
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 
     #[test]
     fn test() -> Result<(), TenetError> {
